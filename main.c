7f06f66fccda20f1f42f7073d584cdae14f084df
Raphael Cazenave-Leveque <raphael.cazenave@gmail.com>
2015-03-01 15:48:02 +0100

    init

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+
+int main (int argc, char **argv) {
+
+    // sqlite *sqlite_open(const char *dbname, int mode, char **errmsg);
+
+    printf("bonjour\n");
+    exit(EXIT_SUCCESS);
+}
